078bb6da4d80a6b26533c4d1e512352335336755
Sam Xie <samxiexs@gmail.com>
2025-12-12 12:49:57 +0800

    Clean commit without secrets

--- common/connect_config.py
+++ common/connect_config.py
@@ -1,13 +1,13 @@
 # upload
-upload_oss_access_key_id = 'ALIYUN_ACCESS_KEY_ID_PLACEHOLDER'
-upload_oss_access_key_secret = 'ALIYUN_ACCESS_KEY_SECRET_PLACEHOLDER'
+upload_oss_access_key_id = ''
+upload_oss_access_key_secret = ''
 upload_oss_endpoint = "oss-cn-beijing-internal.aliyuncs.com"
-upload_oss_bucket_name = "dnnmodel"
+upload_oss_bucket_name = ""
 
 # aliyun/downodps/odps_util/generate_body
 your_default_project = "adx_dmp"
-your_accesskey_id = "ALIYUN_ACCESS_KEY_ID_PLACEHOLDER"
-your_accesskey_secret = "ALIYUN_ACCESS_KEY_SECRET_PLACEHOLDER"
+your_accesskey_id = ""
+your_accesskey_secret = ""
 tunnel_endpoint = "http://dt.cn-beijing.maxcompute.aliyun-inc.com"
 your_end_point = "http://service.cn-beijing.maxcompute.aliyun-inc.com/api"
 # your_end_point = "http://service.cn-beijing.maxcompute.aliyun.com/api"
